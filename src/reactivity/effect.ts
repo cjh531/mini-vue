8c80eb3880ea0b49a990c6ea9f3b3f62a75a63de
cjh531 <13688939119@163.com>
2022-08-20 16:02:05 +0800

    feat: effect & reactive & 依赖收集 & 触发依赖

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,41 @@
+class ReactiveEffect {
+  private _fn: any;
+  constructor(fn) {
+    this._fn = fn;
+  }
+  run() {
+    activeEffect = this;
+    this._fn();
+  }
+}
+
+const targetMap = new WeakMap();
+export function track(target, key) {
+  let depMap = targetMap.get(target);
+  if (!depMap) {
+    depMap = new Map();
+    targetMap.set(target, depMap);
+  }
+
+  let dep = depMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depMap.set(key, dep);
+  }
+
+  dep.add(activeEffect);
+}
+
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target);
+  let dep = depsMap.get(key);
+  for (const effect of dep) {
+    effect.run();
+  }
+}
+
+let activeEffect;
+export function effect(fn) {
+  const _effect = new ReactiveEffect(fn);
+  _effect.run();
+}
