d7e99588fbfd68b8d539f95f0080b0979ed24c8c
cjh531 <13688939119@163.com>
2022-09-21 16:00:22 +0800

    feat: 实现 ref

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -59,19 +59,26 @@ export function track(target, key) {
     depMap.set(key, dep);
   }
 
-  if (dep.has(activeEffect)) return;
+  trackEffects(dep);
+}
 
+export function trackEffects(dep) {
+  if (dep.has(activeEffect)) return;
   dep.add(activeEffect);
   activeEffect.deps.push(dep);
 }
 
-function isTracking() {
+export function isTracking() {
   return shouldTrack && activeEffect !== undefined;
 }
 
 export function trigger(target, key) {
   let depsMap = targetMap.get(target);
   let dep = depsMap.get(key);
+  triggerEffects(dep);
+}
+
+export function triggerEffects(dep) {
   for (const effect of dep) {
     if (effect.scheduler) {
       effect.scheduler();
---
+++ src/reactivity/ref.ts
@@ -0,0 +1,42 @@
+import { hasChanged, isObject } from "../shared";
+import { isTracking, trackEffects, triggerEffects } from "./effect";
+import { reactive } from "./reactive";
+
+class RefImpl {
+  private _value: any;
+  public dep;
+  private _rawValue: any;
+  constructor(value) {
+    this._rawValue = value;
+    this._value = convert(value);
+    this.dep = new Set();
+  }
+  get value() {
+    trackRefValue(this);
+    return this._value;
+  }
+
+  set value(newValue) {
+    if (hasChanged(this._rawValue, newValue)) {
+      this._rawValue = newValue;
+      this._value = convert(newValue);
+      // 在 set 中进行触发依赖
+      triggerEffects(this.dep);
+    }
+  }
+}
+
+function convert(value) {
+  return isObject(value) ? reactive(value) : value;
+}
+
+function trackRefValue(ref) {
+  if (isTracking()) {
+    // 在 get 中进行依赖收集
+    trackEffects(ref.dep);
+  }
+}
+
+export function ref(value) {
+  return new RefImpl(value);
+}
